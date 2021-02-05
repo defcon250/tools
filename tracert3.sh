1ec8b2244b0fa680ceda87f7bd6775b319272815
etherpacket <etherpacket@gmail.com>
2021-02-04 17:25:53 -0800

    updates

--- tracert3.sh
+++ tracert3.sh
@@ -3,7 +3,7 @@
 VAR001=$1
 VAR002=${2:-443}
 
-
+#
 cat <<EOF> /tmp/tracert3.temp 
 VAR001=\$1
 curl -sq  http://ipinfo.io/"\$VAR001" | grep -ivw "readme" | grep -ivw "loc" | grep -iv "timezone" | grep -ivw "postal" | awk -F '{' '{print \$1}' | awk -F '}' '{print \$1}' | xargs 
