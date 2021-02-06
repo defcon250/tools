ffa26e8c6bbf8c4968163eeddb6abdd64582e2da
etherpacket <etherpacket@gmail.com>
2021-02-05 21:30:05 -0800

    updates

--- wifiinfo.sh
+++ wifiinfo.sh
@@ -16,7 +16,7 @@ echo ""
 
 
 echo "---"
-LOCALVER=1.0
+LOCALVER=1.1
 REMOTEVER=$(curl -ks  https://github.com/defcon250/tools/blob/main/wifiinfo.sh | grep -m1 -ioP 'LOCALVER=\w*.?\w*.?\w*.?\w*' | awk -F'=' '{print $2}' | awk -F'<' '{print $1}')
 if [[ $LOCALVER != $REMOTEVER ]]; then echo -e "\033[1;31mupdate $REMOTEVER is avaialble for $0\033[m"; fi | grep -iP '[0-9]' || echo ""
 
