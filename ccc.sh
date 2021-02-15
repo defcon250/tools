07fb4d0a185d6c43ae931a28f1222e60348d6cc9
etherpacket <etherpacket@gmail.com>
2021-02-15 09:47:36 -0800

    updates

--- ccc.sh
+++ ccc.sh
@@ -3,7 +3,7 @@
 echo "---"
 LOCALVER=1.3
 WORKINGDIR=`pwd`
-REMOTEVER=$(curl -H 'Cache-Control: no-cache' -ksq https://raw.githubusercontent.com/defcon250/tools/main/ccc.sh?$(date +%s) 2>/dev/null | grep -m1 'LOCALVER'' | awk -F'=' '{print $2}')
+REMOTEVER=$(curl -H 'Cache-Control: no-cache' -ksq https://raw.githubusercontent.com/defcon250/tools/main/ccc.sh?$(date +%s) 2>/dev/null | grep -m1 'LOCALVER' | awk -F'=' '{print $2}')
 sleep 1
 if [[ $LOCALVER != $REMOTEVER ]]; then echo "Update is available!"; curl -H 'Cache-Control: no-cache' -ksq https://raw.githubusercontent.com/defcon250/tools/main/ccc.sh?$(date +%s) --output /tmp/ccc.sh.temp 2> /dev/null; cd /tmp; chmod +x /tmp/ccc.sh.temp; mv /tmp/ccc.sh.temp $WORKINGDIR/ccc.sh; else echo "This is the latest!"; fi 
 exit
