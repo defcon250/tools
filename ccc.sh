194a5d7edc6a264cad5a554e28e74f0204e1b4d1
etherpacket <etherpacket@gmail.com>
2021-02-14 21:40:40 -0800

    updates

--- ccc.sh
+++ ccc.sh
@@ -3,12 +3,10 @@
 #Uncomment these lines to receive updates info
 echo "---"
 LOCALVER=1.0
+WORKINGDIR=pwd
 REMOTEVER=$(curl -ksq  https://github.com/defcon250/tools/blob/main/ccc.sh 2>/dev/null | grep -m1 -ioP 'LOCALVER=\w*.?\w*.?\w*.?\w*' | awk -F'=' '{print $2}' | awk -F'<' '{print $1}')
 sleep 1
 if [[ $LOCALVER != $REMOTEVER ]]; then echo -e "\033[1;31mupdate $REMOTEVER is avaialble for $0\033[m"; fi | grep -iP '[0-9]' || echo " "
-if [[ $LOCALVER != $REMOTEVER ]]; then wget -q https://github.com/defcon250/tools/blob/main/ccc.sh -O /tmp/ccc.sh.temp 2> /dev/null; fi 
-WORKINGDIR=pwd
-cd /tmp
-mv /tmp/ccc.sh.temp $WORKINGDIR
+if [[ $LOCALVER != $REMOTEVER ]]; then wget -q https://github.com/defcon250/tools/blob/main/ccc.sh -O /tmp/ccc.sh.temp 2> /dev/null; cd /tmp; mv /tmp/ccc.sh.temp $WORKINGDIR; fi 
 exit
 
