b4290f7392e8f78ab33378f2632dcbbf02b33a81
etherpacket <etherpacket@gmail.com>
2021-02-15 11:13:55 -0800

    updates

--- wifiinfo.sh
+++ wifiinfo.sh
@@ -3,13 +3,16 @@
 VAR001=${1:-null} 
 
 
-iw $(iw dev | grep -ioP 'phy#\w*|interface\s\w*'  | grep -i "$VAR001" -B1  | grep -i phy | sed 's/#//g') info 2>/dev/null | egrep -i 'data rate|highest supported' | grep -iv "*" | sort -u | xargs -l
-echo "" 
-VAR_CHANNELS=`iw $(iw dev | grep -ioP 'phy#\w*|interface\s\w*'  | grep -i "$VAR001" -B1  | grep -i phy | sed 's/#//g') info | grep -i Mhz | grep -iv 'radar' | grep -iv disabled | grep -i short  -A 50 | awk -F'[' '{print $2}' | awk -F ']' '{print $1}' | xargs | tr -s ' ' ','` 
-echo "Vendor=`udevadm info /sys/class/net/$VAR001 2>/dev/null | grep -oiP 'id_oui_from_database=.*' | awk -F'=' '{print $2}'`" 
-echo "`udevadm info /sys/class/net/$VAR001 2>/dev/null | grep -oiP 'driver=\w*' | sort -u`" 
-echo "Supported Channels=$VAR_CHANNELS" 
+
+
+iw `iw dev | grep -ioP 'phy#\w*|interface\s\w*'  | grep -i "$VAR001" -B1  | grep -i phy -m1 | sed 's/#//g'` info | grep -i mbps | grep -iv "*" | sort -u | xargs -l
 echo ""
+DEVICE=`iw dev | grep -ioP 'phy#\w*|interface\s\w*'  | grep -i "$VAR001" -B1  | grep -i phy -m1 | sed 's/#//g'` 
+VAR_CHANNELS1=`iw $DEVICE info | grep -i Mhz | grep -iv 'radar' | grep -iv disabled | grep -i short  -A 50 | awk -F'[' '{print $2}' | wc -l`
+VAR_CHANNELS2=`iw $DEVICE info | grep -i Mhz | grep -iv 'radar' | grep -iv disabled | grep -i short  -A 50 | awk -F'[' '{print $2}' | awk -F ']' '{print $1}' | xargs | tr -s ' ' ','`
 
-
-
+echo "$VAR001 Vendor=`udevadm info /sys/class/net/$VAR001 | grep -oiP 'id_oui_from_database=.*' | awk -F'=' '{print $2}'`"
+echo "$VAR001 `udevadm info /sys/class/net/$VAR001 | grep -oiP 'driver=\w*' | sort -u | xargs`"
+echo "`iw reg get | grep -i country`" 
+echo "Supported 5GHz Channels=($VAR_CHANNELS1)=$VAR_CHANNELS2"
+echo ""
