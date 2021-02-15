20af402403bac62bc6503af1f87ebe92e9931e77
etherpacket <etherpacket@gmail.com>
2021-02-15 10:33:28 -0800

    updates

--- ccc.sh
+++ ccc.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+FNAME=`echo $0 | sed -e 's/\.//'`
+echo "$FNAME"
+
 echo "---"
 LOCALVER=1.7
 WORKINGDIR=`pwd`
-REMOTEVER=$(curl -H 'Cache-Control: no-cache' -ksq https://raw.githubusercontent.com/defcon250/tools/main/ccc.sh?$(date +%s) 2>/dev/null | grep -m1 'LOCALVER' | awk -F'=' '{print $2}')
+REMOTEVER=$(curl -H 'Cache-Control: no-cache' -ksq https://raw.githubusercontent.com/defcon250/tools/main/"$FNAME"?$(date +%s) 2>/dev/null | grep -m1 'LOCALVER' | awk -F'=' '{print $2}')
 sleep 1
-if [[ $LOCALVER != $REMOTEVER ]]; then echo "Update is available!"; curl -H 'Cache-Control: no-cache' -ksq https://raw.githubusercontent.com/defcon250/tools/main/ccc.sh?$(date +%s) --output /tmp/ccc.sh.temp 2> /dev/null; cd /tmp; chmod +x /tmp/ccc.sh.temp; mv /tmp/ccc.sh.temp $WORKINGDIR/ccc.sh; else echo "This is the latest!"; fi 
+if [[ $LOCALVER != $REMOTEVER ]]; then echo "Update is available!"; curl -H 'Cache-Control: no-cache' -ksq https://raw.githubusercontent.com/defcon250/tools/main/"$FNAME"?$(date +%s) -o /tmp/"$FNAME".temp 2> /dev/null; cd /tmp; chmod +x /tmp/ccc.sh.temp; mv /tmp/"$FNAME".temp $WORKINGDIR/"$FNAME".sh; else echo "This is the latest!"; fi 
 exit
 
